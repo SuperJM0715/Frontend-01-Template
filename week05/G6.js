3ca71ec9d355c2ce2bc698280a3f6dbb92cf0b67
SuperJM0715 <53811689+SuperJM0715@users.noreply.github.com>
2020-05-13 11:17:32 +0800

    Update G6.js

--- week05/G6.js
+++ week05/G6.js
@@ -1,6 +1,137 @@
+ 把 object 处理成 
+ {
+    "id": "Modeling Methods",
+    "children": [
+        {
+            "id": "Classification",
+            "children": [
+                {
+                    "id": "Logistic regression"
+                },
+              ...
+              ]
+         }
+      ],
+    }
+      的格式
+ 
+  const objects = [
+     "eval",
+     "isFinite",
+     "isNaN",
+     "parseFloat",
+     "parseInt",
+     "decodeURI",
+     "decodeURIComponent",
+     "encodeURI",
+     "encodeURIComponent",
+     "Array",
+     "Date",
+     "RegExp",
+     "Promise",
+     "Proxy",
+     "Map",
+     "WeakMap",
+     "Set",
+     "WeakSet",
+     "Function",
+     "Boolean",
+     "String",
+     "Number",
+     "Symbol",
+     "Object",
+     "Error",
+     "EvalError",
+     "RangeError",
+     "ReferenceError",
+     "SyntaxError",
+     "TypeError",
+     "URIError",
+     "ArrayBuffer",
+     "SharedArrayBuffer",
+     "DataView",
+     "Float32Array",
+     "Float64Array",
+     "Int8Array",
+     "Int16Array",
+     "Int32Array",
+     "Uint8Array",
+     "Uint16Array",
+     "Uint32Array",
+     "Uint8ClampedArray",
+     "Atomics",
+     "JSON",
+     "Math",
+     "Reflect"
+   ];
+
+   const set = new Set();
+
+   const globalObject = {
+     id: "Global Object",
+     children: [
+
+     ]
+   }
+
+   for (let i of objects) {
+     globalObject.children.push({
+       children: [],
+       id: i
+     })
+   }
+
+
+   for (let i = 0; i < objects.length; i++) {
+     const current = objects[i]
+     if (set.has(objects[i]))
+       continue;
+     set.add(objects[i])
+     for (let p of Object.getOwnPropertyNames(window[objects[i]])) {
+       let d = Object.getOwnPropertyDescriptor(window[objects[i]], p)
+       if (d.hasOwnProperty("value") && ((d.value !== null && typeof d.value === "object") || (typeof d.value === "function")) && d.value instanceof Object) {
+         let childrenThird = []
+         for (let k of Object.getOwnPropertyNames(d.value)) {
+           if (k !== 'name' && k !== 'length') {
+             childrenThird.push({ id: k })
+           }
+         }
+         globalObject["children"][i].children.push({
+           children: childrenThird,
+           id: p
+         })
+       }
+       if (d.hasOwnProperty("get") && typeof d.get === "function") {
+         let childrenThird = []
+         for (let k of Object.getOwnPropertyNames(d.get)) {
+           if (k !== 'name' && k !== 'length') {
+             childrenThird.push({ id: k })
+           }
+         }
+         globalObject["children"][i].children.push({
+           children: childrenThird,
+           id: p
+         })
+       }
+       if (d.hasOwnProperty("set") && typeof d.set === "function") {
+         let childrenThird = []
+         for (let k of Object.getOwnPropertyNames(d.set)) {
+           if (k !== 'name' && k !== 'length') {
+             childrenThird.push({ id: k })
+           }
+         }
+         globalObject["children"][i].children.push({
+           children: childrenThird,
+           id: p
+         })
+       }
+     }
+   }
+ 
+
 import G6 from '@antv/g6';
 
-fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/algorithm-category.json')
+fetch(globalObject)
   .then(res => res.json())
   .then(data => {
     const width = document.getElementById('container').scrollWidth;
